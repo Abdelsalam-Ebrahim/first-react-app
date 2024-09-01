ecfc9af48462a01458ef23745432b8947db7b436
Abdelsalam Ebrahim <abdeslalamebrahim21@gmail.com>
2024-09-01 06:38:29 +0300

    handle path

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import Layout from "./Components/Layout/Layout";
 import About from "./Components/About/About";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Contact from "./Components/Contact/Contact";
+import NotFound from "./Components/NotFound/NotFound";
 
 
 const myRouter = createBrowserRouter([
   {path: "", element: <Layout />, children: [
     {index: true, element: <Home />},
+    {path: "first-react-app", element: <Home />},
     {path: "Home", element: <Home />},
     {path: "About", element: <About />},
     {path: "Portfolio", element: <Portfolio />},
     {path: "Contact", element: <Contact />},
+    {path: "*", element: <NotFound />},
   ]}
 ]);
 
---
+++ src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound () {
+    return <>
+
+        <h1 className='text-center d-flex justify-content-center align-items-center' style={{height: "405px"}}> Error 404 Not Found </h1>
+    
+    </>
+}
